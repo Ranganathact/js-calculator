58da79dbd83651703c761de271108c895ff95296
Ranganathact <q6ctranganatha@gmail.com>
2018-11-04 13:14:20 +0530

    intial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function add() {
+    var op1 = document.getElementById("add-op1").value;
+    var op2 = document.getElementById("add-op2").value;
+
+    op1 = parseInt(op1);
+    op2 = parseInt(op2);
+}
+var btnadd=document.getElementById("add");
+btnadd.onclick=add;
+
+
+
+function sub() {
+    var op1 = document.getElementById("sub-op1").value;
+    var op2 = document.getElementById("sub-op2").value;
+    
+    op1 = parseInt(op1);
+    op2 = parseInt(op2);
+}
+var btnadd   
+mul.onclick= function() {
+    alert('mul')
+}
+div.onclick = function() {
+    alert('div')
+}
